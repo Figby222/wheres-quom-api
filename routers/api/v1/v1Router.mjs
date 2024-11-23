917ec3ea6b21b16c4ba5abb940b6a1a9cc4d11a2
Figby222 <figby222@outlook.com>
2024-11-22 20:52:41 -0800

    Use playRouter on /play path

--- routers/api/v1/v1Router.mjs
+++ routers/api/v1/v1Router.mjs
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import * as indexController from "../../../controllers/api/v1/indexController.mjs";
+import playRouter from "./playRouter.mjs";
 
 const indexRouter = Router();
 
+indexRouter.use("/play", playRouter);
 
 indexRouter.get("/", indexController.indexRouteGet);
-
 
 export default indexRouter;
