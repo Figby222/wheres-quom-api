ac2ffdaa7387113e0e5a2cc709a672522cdf9c5a
Figby222 <figby222@outlook.com>
2024-11-26 19:55:09 -0800

    Add Static File Serving

--- index.mjs
+++ index.mjs
@@ -3,11 +3,14 @@ import "dotenv/config";
 import cors from "cors";
 const app = express();
 import routers from "./routers/index.mjs";
+import path from "node:path";
+const __dirname = import.meta.dirname;
 
 app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", routers.apiRouter);
 
 
