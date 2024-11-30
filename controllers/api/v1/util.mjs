9e2a070c2972debf70b88d60f6c42a931d1d54d6
Figby222 <figby222@outlook.com>
2024-11-29 20:47:09 -0800

    Set request game in handleJWTUserAuthorization()
    Set request game instead of user in
    handleJWTUserAuthorization()

--- controllers/api/v1/util.mjs
+++ controllers/api/v1/util.mjs
@@ -2,20 +2,20 @@ import passport from "../../../config/passport.mjs";
 import asyncHandler from "express-async-handler";
 
 const handleJWTUserAuthorization = asyncHandler((req, res, next) => {
-    passport.authenticate("jwt", { session: false }, (err, user, info) => {
+    passport.authenticate("jwt", { session: false }, (err, game, info) => {
         if (err) {
             next(err);
             return;
         }
 
-        if (!user) {
+        if (!game) {
             res.status(401).json({
                 message: "Unauthorized",
             });
             return;
         }
 
-        req.user = user;
+        req.game = game;
         
         next();
     })(req, res, next);
