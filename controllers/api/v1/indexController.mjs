2d3be787d2130a60c9767aa4a20311b86a03a6fc
Figby222 <figby222@outlook.com>
2024-12-05 21:21:46 -0800

    Add test_seed.mjs

--- controllers/api/v1/indexController.mjs
+++ controllers/api/v1/indexController.mjs
@@ -1,7 +1,7 @@
 function indexRouteGet(req, res) {
     res.json({
         message: "Hi",
-    })
+    })a
 }
 
 
---
+++ db/api/v1/test_seed.mjs
@@ -0,0 +1,35 @@
+import pool from "../../pool.mjs";
+
+async function seed() {
+    const characters = await pool.character.createMany({
+        data: [
+            {
+                name: "quom",
+                positionX: 4,
+                positionY: 8,
+                positionX2: 8,
+                positionY2: 14,
+            },
+            {
+                name: "Comal",
+                positionX: 64,
+                positionY: 88,
+                positionX2: 68,
+                positionY2: 94,
+            },
+            {
+                name: "Figby",
+                positionX: 86,
+                positionY: 46,
+                positionX2: 90,
+                positionY2: 53
+            }
+        ]
+    })
+
+    const game = await pool.game.create({
+        startTime: new Date(Date.now()).toISOString()
+    })
+}
+
+seed();
