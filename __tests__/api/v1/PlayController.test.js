28b5ed65d31c151afab27d73737cd390390b2be4
Figby222 <figby222@outlook.com>
2024-12-06 04:46:46 -0800

    Add PlayController.test.js

---
+++ __tests__/api/v1/PlayController.test.js
@@ -0,0 +1,17 @@
+const router = require("../../../routers/api/v1/playRouter.js");
+
+const request = require("supertest");
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use("/", router);
+
+describe("Index route POST", () => {
+    test("Index route works", (done) => {
+        request("app")
+            .get("/")
+            .expect("Content-Type", /json/)
+            .expect(200, done);
+    })
+})
