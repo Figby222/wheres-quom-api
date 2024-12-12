9e37a47a23d64420d81207b7594224c829924945
Figby222 <figby222@outlook.com>
2024-12-12 01:58:59 -0800

    Fix test_seed game creation & position fields

--- db/api/v1/test_seed.js
+++ db/api/v1/test_seed.js
@@ -1,34 +1,38 @@
 const pool = require("../../pool.js");
 
 async function seed() {
+    await pool.character.deleteMany({});
+    await pool.game.deleteMany({});
     const characters = await pool.character.createMany({
         data: [
             {
                 name: "quom",
-                positionX: 4,
-                positionY: 8,
-                positionX2: 8,
-                positionY2: 14,
+                positionLeft: 4,
+                positionTop: 8,
+                positionRight: 8,
+                positionBottom: 14,
             },
             {
                 name: "Comal",
-                positionX: 64,
-                positionY: 88,
-                positionX2: 68,
-                positionY2: 94,
+                positionLeft: 64,
+                positionTop: 88,
+                positionRight: 68,
+                positionBottom: 94,
             },
             {
                 name: "Figby",
-                positionX: 86,
-                positionY: 46,
-                positionX2: 90,
-                positionY2: 53
+                positionLeft: 86,
+                positionTop: 46,
+                positionRight: 90,
+                positionBottom: 53
             }
         ]
     })
 
     const game = await pool.game.create({
-        startTime: new Date(Date.now()).toISOString()
+        data: {
+            startTime: new Date(Date.now()).toISOString()
+        }
     })
 }
 
