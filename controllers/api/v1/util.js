7b91b27a04c2453a112b034631f19fc6a3395f08
Figby222 <figby222@outlook.com>
2025-01-01 18:53:05 -0800

    Return false from target if char 15 percent above
    Return false from targetBoxCharacterCollision() if
    character is 15 pixel percentages above targetBox

--- controllers/api/v1/util.js
+++ controllers/api/v1/util.js
@@ -23,7 +23,7 @@ const handleJWTGameAuthorization = asyncHandler((req, res, next) => {
 
 const targetBoxCharacterCollision = (targetBoxXPercentage, targetBoxYPercentage, characterCoordinatePercentages) => {
     console.log(targetBoxYPercentage);
-    if ((targetBoxXPercentage > characterCoordinatePercentages.right || targetBoxXPercentage + 10 < characterCoordinatePercentages.left) || (targetBoxYPercentage + 10 < characterCoordinatePercentages.top || targetBoxYPercentage === 34)) {
+    if ((targetBoxXPercentage > characterCoordinatePercentages.right || targetBoxXPercentage + 10 < characterCoordinatePercentages.left) || (targetBoxYPercentage + 10 < characterCoordinatePercentages.top || targetBoxYPercentage === characterCoordinatePercentages.top + 15)) {
         return false;
     }
     
