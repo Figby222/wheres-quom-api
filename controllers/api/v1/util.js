a50c302f6d55e364f6fb13815f2d6625252ce3e6
Figby222 <figby222@outlook.com>
2025-01-03 19:23:17 -0800

    Return true from targeting if tallest is covered
    Return true from targetBoxCharacterCollision()
    when tallest character is partially covered

--- controllers/api/v1/util.js
+++ controllers/api/v1/util.js
@@ -23,7 +23,7 @@ const handleJWTGameAuthorization = asyncHandler((req, res, next) => {
 
 const targetBoxCharacterCollision = (targetBoxXPercentage, targetBoxYPercentage, characterCoordinatePercentages) => {
     console.log(targetBoxYPercentage);
-    if ((targetBoxXPercentage > characterCoordinatePercentages.right || targetBoxXPercentage + 10 < characterCoordinatePercentages.left) || (targetBoxYPercentage + 10 < characterCoordinatePercentages.top || targetBoxYPercentage > characterCoordinatePercentages.top + 11)) {
+    if ((targetBoxXPercentage > characterCoordinatePercentages.right || targetBoxXPercentage + 10 < characterCoordinatePercentages.left) || (targetBoxYPercentage + 10 < characterCoordinatePercentages.top || targetBoxYPercentage > characterCoordinatePercentages.bottom)) {
         return false;
     }
     
