47ec9cce88240a1429ce7d0268dcdae7d150da85
Figby222 <figby222@outlook.com>
2025-01-18 15:34:34 -0800

    Use leaderboardRouter on /leaderboard path

--- routers/api/v1/v1Router.js
+++ routers/api/v1/v1Router.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const indexController = require("../../../controllers/api/v1/indexController.js");
 const playRouter = require("./playRouter.js");
+const leaderboardRouter = require("./leaderboardRouter.js");
 
 const indexRouter = Router();
 
 indexRouter.use("/play", playRouter);
+indexRouter.use("/leaderboard", leaderboardRouter);
 
 indexRouter.get("/", indexController.indexRouteGet);
 
