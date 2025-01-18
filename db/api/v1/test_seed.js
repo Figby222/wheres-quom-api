cbafccb02a18f669c463045a0c6016124ec678e3
Figby222 <figby222@outlook.com>
2025-01-17 20:46:44 -0800

    Populate test database with leaderboard seeds

--- db/api/v1/test_seed.js
+++ db/api/v1/test_seed.js
@@ -1,5 +1,7 @@
 const pool = require("../../pool.js");
 
+const ONE_SECOND_IN_MILLISECONDS = 1000;
+
 async function seed() {
     await pool.character.deleteMany({});
     await pool.game.deleteMany({});
@@ -32,10 +34,50 @@ async function seed() {
         ]
     })
 
-    const game = await pool.game.create({
-        data: {
-            startTime: new Date(Date.now()).toISOString()
-        }
+    const game = await pool.game.createMany({
+        data: [
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 10).toISOString()
+            },
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 20).toISOString() 
+            },
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 30).toISOString()
+            },
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 40).toISOString()
+            },
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 50).toISOString()
+            },
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 60).toISOString()
+            },
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 70).toISOString()
+            },
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 80).toISOString()
+            },
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 90).toISOString()
+            },
+            {
+                startTime: new Date(Date.now()).toISOString(),
+                endTime: new Date(Date.now() + ONE_SECOND_IN_MILLISECONDS * 100).toISOString()
+            },
+
+        ]
     })
 }
 
