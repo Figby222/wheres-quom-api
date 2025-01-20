eff41d326d8abb6ccae899d6822703468f7156ec
Figby222 <figby222@outlook.com>
2025-01-19 17:27:12 -0800

    Add quom leaderboard name in leaderboardPut()

--- controllers/api/v1/leaderboardController.js
+++ controllers/api/v1/leaderboardController.js
@@ -1,5 +1,6 @@
 const db = require("../../../db/api/v1/leaderboardQueries.js");
 const asyncHandler = require("express-async-handler");
+const utils = require("./util.js");
 
 const leaderboardGet = asyncHandler(async (req, res) => {
     const leaderboard = await db.getLeaderboard();
@@ -7,8 +8,13 @@ const leaderboardGet = asyncHandler(async (req, res) => {
     res.status(200).json(leaderboard);
 })
 
-const leaderboardPut = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Hi", isTop10: true });
-})
+const leaderboardPut = [
+    utils.handleJWTGameAuthorization,
+    asyncHandler(async (req, res) => {
+        const game = await db.addLeaderboardName(req.game.id, "quom");
+
+        res.status(200).json({ message: "Hi", isTop10: true });
+    })
+]
 
 module.exports = { leaderboardGet, leaderboardPut };
