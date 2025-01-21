e36d89933a4ec2c3c7a06a15a42dfb09c8f71fd1
Figby222 <figby222@outlook.com>
2025-01-20 16:28:38 -0800

    Add validatePut() form validation

--- controllers/api/v1/leaderboardController.js
+++ controllers/api/v1/leaderboardController.js
@@ -1,6 +1,13 @@
 const db = require("../../../db/api/v1/leaderboardQueries.js");
 const asyncHandler = require("express-async-handler");
 const utils = require("./util.js");
+const { body, validationResult } = require("express-validator");
+
+const validatePut = [
+    body("username")
+        .toString()
+        .length({ min: 5, max: 14 })
+]
 
 const leaderboardGet = asyncHandler(async (req, res) => {
     const leaderboard = await db.getLeaderboard();
@@ -9,8 +16,17 @@ const leaderboardGet = asyncHandler(async (req, res) => {
 })
 
 const leaderboardPut = [
+    validatePut,
     utils.handleJWTGameAuthorization,
     asyncHandler(async (req, res) => {
+        const errorsResult = validationResult(req);
+        if (!errorsResult.isEmpty()) {
+            return res.status(400).json({
+                errors: {
+                    ...errorsResult.errors
+                }
+            })
+        }
         const game = await db.addLeaderboardName(req.game.id, "quom");
 
         res.status(200).json({ message: "Hi", isTop10: true });
