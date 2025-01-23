c712fcdb21521f03f169c1f50d8518178c18b95e
Figby222 <figby222@outlook.com>
2025-01-23 14:10:45 -0800

    Make minimum username 4

--- controllers/api/v1/leaderboardController.js
+++ controllers/api/v1/leaderboardController.js
@@ -5,7 +5,7 @@ const { body, validationResult } = require("express-validator");
 
 const validatePut = [
     body("username")
-        .isLength({ min: 5, max: 14 })
+        .isLength({ min: 4, max: 14 })
 ]
 
 const leaderboardGet = asyncHandler(async (req, res) => {
